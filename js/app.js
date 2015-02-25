d45b0050f7652f250eb010fade03b0b138c4c982
rcdavis4 <rcdavis4@gmail.com>
2015-02-25 06:26:23 -0800

    get user guess

--- js/app.js
+++ js/app.js
@@ -1,17 +1,43 @@
 
+
+
+
+
+
+
 $(document).ready(function(){
 	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+  /*--- Display information modal box ---*/
+  $(".what").click(function(){
+    $(".overlay").fadeIn(1000);
+  });
 
-  	});
+  /*--- Hide information modal box ---*/
+  $("a.close").click(function(){
+    $(".overlay").fadeOut(1000);
+  });
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
 
-});
+  var userGuess = getUserGuess();
+  console.log('user guess: ' + userGuess);
+  console.log('guesses: ' + numOfGuesses);
 
+}); // end document.ready
+
+
+var numOfGuesses = 0;
+
+function newGame() {
+  var count = 0;
+}
+
+function getUserGuess() {
+  $('#guessButton').click(function() {
+    var userGuess = $('#userGuess').val();
+    userGuess = Number(userGuess);
+    numOfGuesses++;
+
+  return userGuess;
+  })
+}
 
